97b3a6edcc8e397882f4b006ca3d7b1700daa593
Almir666 <106429048+Almir666@users.noreply.github.com>
2024-10-24 18:57:38 +0300

    initial commit

---
+++ src/main/java/com/telegram/quiz/quizbot/QuizBotApplication.java
@@ -0,0 +1,13 @@
+package com.telegram.quiz.quizbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/telegram/quiz/quizbot/QuizBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telegram.quiz.quizbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
