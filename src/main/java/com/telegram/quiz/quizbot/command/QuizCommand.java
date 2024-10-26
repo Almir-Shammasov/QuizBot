1a293886960de9b1231a455651cf8e41b956842f
Almir666 <106429048+Almir666@users.noreply.github.com>
2024-10-27 00:15:51 +0300

    Реализовал логику получения вопросов без повторения

--- src/main/java/com/telegram/quiz/quizbot/command/QuizCommand.java
+++ src/main/java/com/telegram/quiz/quizbot/command/QuizCommand.java
@@ -10,6 +10,8 @@ import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.polls.SendPoll;
 import org.telegram.telegrambots.meta.api.objects.Message;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Component
@@ -17,7 +19,16 @@ import java.util.List;
 public class QuizCommand implements Command {
     private final TelegramService telegramService;
     private final DbQuizService dbQuizService;
-    private List<Integer> questions;
+    private final List<Integer> questions = new ArrayList<>();
+
+
+    public void fillQuestions() {
+        int quizSize = dbQuizService.getAllQuestions().size();
+        for (int i = 1; i <= quizSize; i++) {
+            questions.add(i);
+        }
+        Collections.shuffle(questions);
+    }
 
     @Override
     public String getCommandName() {
@@ -28,8 +39,10 @@ public class QuizCommand implements Command {
     @Transactional
     public void execute(Message message) {
         Long chatId = message.getChatId();
-        int quizSize = dbQuizService.getAllQuestions().size() - 1;
-        int randomQuestion = (int) (Math.random() * ++quizSize) + 1;
+        if (questions.isEmpty()) {
+            fillQuestions();
+        }
+        int randomQuestion = questions.removeFirst();
         QuizObject randomQuiz = dbQuizService.getById(randomQuestion);
         SendPoll quiz = TelegramUtils.createQuiz(chatId, randomQuiz.getTitle(), randomQuiz.getAnswers(), randomQuiz.getCorrectindex());
         telegramService.sendPoll(quiz);
